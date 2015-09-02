26abafe639920e19e0afe688970fa03e3dfc2d57
Tribex <joshua@derfers.us>
2015-09-02 15:02:59 +0800

    New logo | Minor style changes

--- src/backend.js
+++ src/backend.js
@@ -1,9 +1,36 @@
+// COPYRIGHT (c) 2015 Joshua Bemenderfer
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Background process for iBrows
+
 var app = require('app')
 var BrowserWindow = require('browser-window')
 
 require('crash-reporter').start()
 
 var mainWindow = null
+
+app.commandLine.appendSwitch('enable-transparent-visuals')
 
 app.on('window-all-closed', function () {
   app.quit()
@@ -16,6 +43,7 @@ app.on('ready', function () {
   mainWindow = new BrowserWindow({
     show: false,
     frame: false,
+    transparent: true,
     icon: __dirname + '/images/icon.png',
     x: 0,
     y: 0,
